5c3b1e20b14a6fe0baa7e6ab256e3552fb2e7dad
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-08-15 00:51:54 -0300

    Exercícios 1 e 2 - Lista 1

---
+++ exercicio-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  int n1, n2, n3, n4, soma;
+  printf("Digite o primeiro número: ");
+  scanf("%d", &n1);
+
+  printf("Digite o segundo número: ");
+  scanf("%d", &n2);
+
+  printf("Digite o terceiro número: ");
+  scanf("%d", &n3);
+
+  printf("Digite o quarto número: ");
+  scanf("%d", &n4);
+
+  soma = n1 + n2 + n3 + n4;
+
+  printf("A soma dos valores é %d", soma);
+}
---
+++ exercicio-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+  float nota1, nota2, nota3, soma, media;
+
+  printf("Insira a primeira nota ");
+  scanf("%f", &nota1);
+
+  printf("Insira a segunda nota ");
+  scanf("%f", &nota2);
+
+  printf("Insira a terceira nota ");
+  scanf("%f", &nota3);
+
+  soma = nota1 + nota2 + nota3;
+
+  printf("A media aritmetica eh %f", soma / 3);
+}
