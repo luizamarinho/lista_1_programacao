4e16b1d1f6f050e57f450b8b7dae9cbbec956d08
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-08-20 19:27:01 -0300

    exercicios 5 e 6

---
+++ exercicio-5.c
@@ -0,0 +1,19 @@
+// Faça um programa que receba o salário de um 
+// funcionário e o percentual de aumento, calcule e 
+// mostre o valor do aumento e o novo salário.
+
+#include <stdio.h>
+
+int main() {
+  float salario, aumento, salario_ajust;
+  printf("Digite o valor do salário ");
+  scanf("%f", &salario);
+
+  printf("Digite a porcentagem de aumento ");
+  scanf("%f", &aumento);
+
+  salario_ajust = salario + (salario * (aumento/100));
+
+  printf("O salário com aumento é %5.2f", salario_ajust);
+}
+
---
+++ exercicio-6.c
@@ -0,0 +1,18 @@
+// Faça um programa que calcule e mostre a área de 
+// um triângulo. Sabe-se que: Área = (base * altura)/2.
+
+#include <stdio.h>
+
+int main(){
+    float base, altura, area;
+
+    printf("Digite a base do triangulo ");
+    scanf("%f", &base);
+
+    printf("Digite a altura do triangulo ");
+    scanf("%f", &altura);
+
+    area = (base * altura) / 2;
+
+    printf("A area do triangulo eh %f", area);
+}
